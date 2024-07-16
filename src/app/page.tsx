bc2274a83650c0ba38a7d463b6072fa3e4d9d0c4
shivendra <shivendrakeshari581@gmail.com>
2024-07-16 17:36:10 +0530

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
 return(
+ 
+     
+     <h1 className="text-center text-3xl" >class</h1>
 
-<h1 className="text-2xl text-center" > hello jee</h1>
-
-  
+     
+     
 );
 }
